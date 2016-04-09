47ad7530ccb1d8594ad90aa8aa572cdfa17d4710
Thor Helms <thor@ittr.dk>
2016-04-09 15:37:01 +0200

    Added winning text

--- Assets/Scripts/GameMaster/GameMaster.cs
+++ Assets/Scripts/GameMaster/GameMaster.cs
@@ -5,24 +5,59 @@ using System.Collections.Generic;
 
 public class GameMaster : MonoBehaviour {
 
-	public List<Boat> playerBoats;
-	public List<Text> playerScores;
-	public List<Text> playerRefugeeCounters;
+	public string[] playerNames;
+	public Boat[] playerBoats;
+	public Text[] playerScores;
+	public Text[] playerRefugeeCounters;
+	public int goalScore;
+	public Text winningText;
 
 	// Use this for initialization
 	void Start () {
-	
+		ResetGame();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		for (var i = 0; i < playerBoats.Count; i++) {
-			if (playerScores.Count > i) {
-				playerScores[i].text = String.Format("${0}", playerBoats[i].Score);
+		var winningPlayers = new List<string>();
+
+		for (var i = 0; i < playerBoats.Length; i++) {
+			if (playerBoats[i] == null) {
+				continue;
 			}
-			if (playerRefugeeCounters.Count > i) {
+			if (playerScores.Length > i) {
+				var score = playerBoats[i].Score;
+				if (score > goalScore) {
+					winningPlayers.Clear();
+					goalScore = score;
+				}
+				if (score >= goalScore) {
+					var playerName = playerNames[i];
+					if (String.IsNullOrEmpty(playerName)) {
+						playerName = String.Format("Unknown player {0}", i);
+					}
+					winningPlayers.Add(playerName);
+				}
+				playerScores[i].text = String.Format("${0}", score);
+			}
+
+			if (playerRefugeeCounters.Length > i) {
 				playerRefugeeCounters[i].text = String.Format("Refugees: {0}", playerBoats[i].RefugeeContainer.GetCount());
 			}
 		}
+
+		if (winningPlayers.Count > 0) {
+			// Game over!
+			winningText.enabled = true;
+			var winnerText = "Game over!\n";
+			winnerText += winningPlayers.Count == 1 ? "Winner:" : "Winners:";
+			foreach (var winningPlayer in winningPlayers) {
+				winnerText += "\n" + winningPlayer;
+			}
+		}
+	}
+
+	public void ResetGame() {
+		winningText.enabled = false;
 	}
 }
