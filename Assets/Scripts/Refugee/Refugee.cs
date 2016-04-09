50f2c7609a5b0145896de9b32ada356ce75059a7
Scrattlebeard <juhl.daniel@gmail.com>
2016-04-09 18:01:44 +0200

    Fixed a build error

--- Assets/Scripts/Refugee/Refugee.cs
+++ Assets/Scripts/Refugee/Refugee.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 namespace Assets.Scripts.Refugee
 {
-    public class Refugee : MonoBehaviour {
+    public class Refugee : MonoBehaviour
+    {
 
         public int Value;
         public bool Drowning;
@@ -21,49 +22,52 @@ namespace Assets.Scripts.Refugee
                     _destination = destinations[Random.Range(0, destinations.Length)];
                 }
                 return _destination;
-            } 
+            }
         }
 
         private double _lifetime;
 
         // Use this for initialization
-        void Start () {
+        void Start()
+        {
 
             Value = Constants.DefaultValues.RefugeeValue;
             _lifetime = Constants.DefaultValues.RefugeeLifespan;
-            Drowning = false;            
+            Drowning = false;
         }
-	
+
         // Update is called once per frame
-        void Update () {
-
-        //Only update the living
-        if (Drowning)
+        void Update()
         {
-            //Decrease liftime
-            _lifetime -= Time.deltaTime;
 
-            //Are we dead yet?
-            if (_lifetime <= 0)
+            //Only update the living
+            if (Drowning)
             {
-                Destroy(this);
+                //Decrease liftime
+                _lifetime -= Time.deltaTime;
+
+                //Are we dead yet?
+                if (_lifetime <= 0)
+                {
+                    Destroy(this);
+                }
             }
         }
-	}
 
-    /// <summary>
-    /// Tells the refugee that he has been dumped of the boat, you heartless bastard.
-    /// </summary>
-    public void Dump()
-    {
-        Drowning = true;
-    }
+        /// <summary>
+        /// Tells the refugee that he has been dumped of the boat, you heartless bastard.
+        /// </summary>
+        public void Dump()
+        {
+            Drowning = true;
+        }
 
-    /// <summary>
-    /// Save a refugee from the sea by inviting on board. What a nice person you are.
-    /// </summary>
-    public void PickUp()
-    {
-        Drowning = false;
+        /// <summary>
+        /// Save a refugee from the sea by inviting on board. What a nice person you are.
+        /// </summary>
+        public void PickUp()
+        {
+            Drowning = false;
+        }
     }
 }
